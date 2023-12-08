4b084b023863c5ca3175dab07660ea35dcf21cb3
alejandriadavila <149810746+alejandriadavila@users.noreply.github.com>
2023-12-08 12:45:39 -0500

    valid course id, grouped learnerSubmissions by learnerId, caluclate grade of each assignment

--- example.js
+++ example.js
@@ -1,102 +1,133 @@
 // The provided course information.
-const CourseInfo = {
-    id: 451,
-    name: "Introduction to JavaScript"
-  };
-  
-  // The provided assignment group.
-  const AssignmentGroup = {
-    id: 12345,
-    name: "Fundamentals of JavaScript",
-    course_id: 451,
-    group_weight: 25,
-    assignments: [
-      {
-        id: 1,
-        name: "Declare a Variable",
-        due_at: "2023-01-25",
-        points_possible: 50
-      },
-      {
-        id: 2,
-        name: "Write a Function",
-        due_at: "2023-02-27",
-        points_possible: 150
-      },
-      {
-        id: 3,
-        name: "Code the World",
-        due_at: "3156-11-15",
-        points_possible: 500
-      }
-    ]
-  };
-  
-  // The provided learner submission data.
-  const LearnerSubmissions = [
+const courseInfo = {
+  courseId: 451,
+  name: "Introduction to JavaScript",
+};
+
+// The provided assignment group.
+const assignmentGroup = {
+  id: 12345,
+  name: "Fundamentals of JavaScript",
+  courseId: 451,
+  groupWeight: 25,
+  assignments: [
     {
-      learner_id: 125,
-      assignment_id: 1,
-      submission: {
-        submitted_at: "2023-01-25",
-        score: 47
-      }
+      id: 1,
+      name: "Declare a Variable",
+      dueAt: "2023-01-25",
+      pointsPossible: 50,
     },
     {
-      learner_id: 125,
-      assignment_id: 2,
-      submission: {
-        submitted_at: "2023-02-12",
-        score: 150
-      }
+      id: 2,
+      name: "Write a Function",
+      dueAt: "2023-02-27",
+      pointsPossible: 150,
     },
     {
-      learner_id: 125,
-      assignment_id: 3,
-      submission: {
-        submitted_at: "2023-01-25",
-        score: 400
-      }
+      id: 3,
+      name: "Code the World",
+      dueAt: "3156-11-15",
+      pointsPossible: 500,
     },
-    {
-      learner_id: 132,
-      assignment_id: 1,
-      submission: {
-        submitted_at: "2023-01-24",
-        score: 39
-      }
+  ],
+};
+
+// The provided learner submission data.
+const learnerSubmissions = [
+  {
+    learnerId: 125,
+    assignmentId: 1,
+    submission: {
+      submittedAt: "2023-01-25",
+      score: 47,
     },
-    {
-      learner_id: 132,
-      assignment_id: 2,
-      submission: {
-        submitted_at: "2023-03-07",
-        score: 140
+  },
+  {
+    learnerId: 125,
+    assignmentId: 2,
+    submission: {
+      submittedAt: "2023-02-12",
+      score: 150,
+    },
+  },
+  {
+    learnerId: 125,
+    assignmentId: 3,
+    submission: {
+      submittedAt: "2023-01-25",
+      score: 400,
+    },
+  },
+  {
+    learnerId: 132,
+    assignmentId: 1,
+    submission: {
+      submittedAt: "2023-01-24",
+      score: 39,
+    },
+  },
+  {
+    learnerId: 132,
+    assignmentId: 2,
+    submission: {
+      submittedAt: "2023-03-07",
+      score: 140,
+    },
+  },
+];
+
+function getLearnerData(course, ag, submissions) {
+  let result = [];
+
+  // Check that courseid matches from course & ag
+  if (course.courseId === ag.courseId) {
+    // Followed documentation for object.values() found here: https://www.geeksforgeeks.org/how-to-group-objects-in-an-array-based-on-a-common-property-into-an-array-of-arrays-in-javascript/
+    const groupByLearnerId = Object.values(
+      submissions.reduce((group, current) => {
+        group[current.learnerId] = group[current.learnerId] ?? [];
+        group[current.learnerId].push(current);
+        return group;
+      }, {})
+    );
+
+      // Add learnerId and avg to result
+      for (let i = 0; i < groupByLearnerId.length; i++) {
+        let learner = {};
+        learner.id = groupByLearnerId[i].learnerId;
+        result.push({ id: groupByLearnerId[i][0].learnerId, avg: 0 });
+      }
+  // calculate grade of each assignment
+  let grade = [];
+  for (const i in groupByLearnerId) {
+    // console.log(groupByLearnerId[i])
+    for (const j in groupByLearnerId[i]) {
+      // grade.push({assignmentId: groupByLearnerId[i][j].assignmentId})
+      // console.log({assignmentId: groupByLearnerId[i][j].assignmentId})
+      for (const k in ag.assignments) {
+        // console.log(ag.assignments[k]);
+        // function isAssignmentIdEqual(object1, object2) {
+        //   if(object1.assignmentId === object2.id)
+        // console.log(`${object1} === ${object2}???`)
+        // return true
+        // }
+        if (groupByLearnerId[i][j].assignmentId == ag.assignments[k].id) {
+          grade.push({
+            assignment:
+              groupByLearnerId[i][j].submission.score /
+              ag.assignments[k].pointsPossible,
+          });
+        } else {
+          continue;
+        }
       }
     }
-  ];
-  
-  function getLearnerData(course, ag, submissions) {
-    // here, we would process this data to achieve the desired result.
-    const result = [
-      {
-        id: 125,
-        avg: 0.985, // (47 + 150) / (50 + 150)
-        1: 0.94, // 47 / 50
-        2: 1.0 // 150 / 150
-      },
-      {
-        id: 132,
-        avg: 0.82, // (39 + 125) / (50 + 150)
-        1: 0.78, // 39 / 50
-        2: 0.833 // late: (140 - 15) / 150
-      }
-    ];
-  
-    return result;
+    result.push(grade);
+
+      console.log(result);
+      // }else{
+      // throw err "Error- Course ID's do not match"
+      // }
+    }
   }
-  
-  const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
-  
-  console.log(result);
-  
+}
+getLearnerData(courseInfo, assignmentGroup, learnerSubmissions);
